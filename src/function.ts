2709d0837289b4a9e21c67d23e0381815f599bc4
K M Marziul Karim <marziulrafi@gmail.com>
2025-11-11 23:44:28 +0600

    Method

--- src/function.ts
+++ src/function.ts
@@ -8,3 +8,24 @@ const addArrow = (num1: number, num2: number): number => num1 + num2;
 
 console.log(addArrow(43, 235))
 console.log(addNormal(734, 21))
+
+
+
+
+// Object -> Function -> Method
+
+const user = {
+    name: "Marziul",
+    balance: 0,
+    addBalance(value: number): number {
+        const totalBalance = this.balance + value;
+        return totalBalance;
+    },
+};
+
+user.addBalance(100000);
+
+
+
+const arr: number[] = [1, 4, 6];
+const sqrArray = arr.map((el: number): number => el * el);
