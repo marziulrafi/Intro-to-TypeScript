8c6e5b94370fe9d9f9fc29796f6d55579058fc4f
K M Marziul Karim <marziulrafi@gmail.com>
2025-11-12 21:06:50 +0600

    Array Destructuring

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -21,3 +21,18 @@ const {
 
 console.log(favouriteColor);
 console.log(myMiddleName);
+
+
+
+
+// Array Destructuring
+
+const friends = ["Messi", "Suarez", "Neymar"];
+
+const [my1stFriend] = friends;
+const [, my2ndFriend] = friends;
+const [, , my3rdFriend] = friends;
+
+console.log(my1stFriend);
+console.log(my2ndFriend);
+console.log(my3rdFriend);
